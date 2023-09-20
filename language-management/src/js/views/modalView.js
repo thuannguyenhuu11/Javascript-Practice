ad18a4477fed5f75d44abde23a06d38b9fe41f60
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2023-09-20 12:31:14 +0700

    Update logic for modalView

--- language-management/src/js/views/modalView.js
+++ language-management/src/js/views/modalView.js
@@ -8,19 +8,27 @@ class ModalView {
     this.modalEl = document.querySelector(".modal");
     this.overlayEl = document.querySelector(".overlay");
     this.addBtnEl = document.querySelector(".features__add");
-    this.cancelBtnEl = document.querySelector(".modal__top__btn");
+    this.cancelBtnEl = document.querySelector(".modal__buttons__cancel");
 
-    this.addBtnEl.addEventListener("click", () => {
-      this.modalEl.classList.add("modal--active");
-      this.overlayEl.classList.add("overlay--active");
-      this.openEvent.trigger();
-    });
+    // Attach event to the New-button using the handleOpenAddModal method
+    this.addBtnEl.addEventListener("click", this.handleOpenAddModal.bind(this));
 
-    this.cancelBtnEl.addEventListener("click", () => {
-      this.modalEl.classList.remove("modal--active");
-      this.overlayEl.classList.remove("overlay--active");
-      this.closeEvent.trigger();
-    });
+    // Attach event to the Cancel-button using the handleCloseAddModal method
+    this.cancelBtnEl.addEventListener("click", this.handleCloseAddModal.bind(this));
+  }
+
+  // Handle open ADD-modal
+  handleOpenAddModal() {
+    this.modalEl.classList.add("modal--active");
+    this.overlayEl.classList.add("overlay--active");
+    this.openEvent.trigger();
+  }
+
+  // Handle close ADD-modal
+  handleCloseAddModal() {
+    this.modalEl.classList.remove("modal--active");
+    this.overlayEl.classList.remove("overlay--active");
+    this.closeEvent.trigger();
   }
 
   //Handle render logic
