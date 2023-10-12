38c44521b467d16b94978694a9bafd45ecb343d7
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2023-10-12 20:42:09 +0700

    Fix model tag

--- address-book-client/src/js/models/model.js
+++ address-book-client/src/js/models/model.js
@@ -1,23 +1,8 @@
-import Event from "../events/event";
+import Card from "./contact";
+
 class Model {
   constructor() {
-    this.openContactEvent = new Event();
-    this.closeModalEvent = new Event();
-    this.handleFormEvent = new Event();
-  }
-
-  open() {
-    // logic for opening modal add-contact.
-    this.openModalEvent.trigger();
-  }
-
-  close() {
-    // logic for closing modal add-contact.
-    this.closeModalEvent.trigger();
-  }
-
-  handle() {
-    this.handleFormEvent.trigger();
+    this.card = new Card();
   }
 }
 
