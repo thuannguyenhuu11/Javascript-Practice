2ba0dcc4644ec56ce92f854246777f75b99653ac
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2023-11-23 18:36:31 +0700

    Update url

--- json-server/server.js
+++ json-server/server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; //You can use any port number here; i choose to use 3001
+const port = process.env.PORT || 3001; //You can use any port number here; i choose to use 3000
 
 server.use(middlewares);
 server.use(router);
