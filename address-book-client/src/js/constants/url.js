4d30aa9d6e9da606798a2f6ac3cc66fe5b3dcb59
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2023-12-01 17:57:33 +0700

    Change url

--- address-book-client/src/js/constants/url.js
+++ address-book-client/src/js/constants/url.js
@@ -2,7 +2,6 @@
  * API gateway url
  * The default value is 'http://localhost:3000'.
  */
-// const API_BASE_URL = 'https://address-book-api.onrender.com';
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://address-book-api.onrender.com';
 
 export default API_BASE_URL;
